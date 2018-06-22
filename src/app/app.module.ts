434766f7da95c2abefb1bd6f98393cc418a77f93
amit <amitk0055@gmail.com>
2018-06-22 16:51:47 +0530

    c

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Router,RouterModule, Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ExamComponent } from './exam/exam.component';
+import { InstructionComponent } from './instruction/instruction.component';
+import { Instruction2Component } from './instruction2/instruction2.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { QuizComponent } from './quiz/quiz.component';
+const appRout:Routes=[
+  {path: '', redirectTo: '/Login', pathMatch: 'full'},
+  {path:'Login',component:ExamComponent},
+  {path:'instruction', component:InstructionComponent},
+  {path:'instructions', component:Instruction2Component},
+  {path:'quiz', component:QuizComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ExamComponent,
+    InstructionComponent,
+    Instruction2Component,
+    UserProfileComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRout),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/instruction2/instruction2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instruction2',
+  templateUrl: './instruction2.component.html',
+  styleUrls: ['./instruction2.component.css']
+})
+export class Instruction2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
